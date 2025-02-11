af422ccfaee38938993e189710ab375fa173816d
Jacinto Mba Cantero <jacmba@gmail.com>
2025-02-11 17:58:31 +0000

    Added application logo

--- UI/main.cpp
+++ UI/main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QIcon icon(":/images/logo.png");
+    a.setWindowIcon(icon);
     MainWindow w;
     w.show();
     return a.exec();
